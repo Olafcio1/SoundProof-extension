c5b6f060f796cc60979a00ddb95175e500294e15
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2026-09-18 18:40:46 +0200

    Update Hubs.js

--- Platforms/YtMusic/Hubs.js
+++ Platforms/YtMusic/Hubs.js
@@ -5,4 +5,4 @@ UCMOgdURr7d8pOVlc-alkfRg  //  @xKitoMusic
 UC65afEgL62PGFWXY7n6CUbA  //  @trapcity
 UCCvVpbYRgYjMN7mG7qQN0Pg  //  @808nation
 
-`.trim().split("\n").map(l => l.split("//")[0]);
+`.trim().split("\n").map(l => l.split("//")[0].trim());
