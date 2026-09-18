99a30d46a99c638dd0a18a5d9fa47a452d69bc35
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2026-09-18 17:37:07 +0200

    Update YtMusic.js

--- Platforms/YtMusic.js
+++ Platforms/YtMusic.js
@@ -16,13 +16,30 @@ function checkAndUpdateBadge() {
     const titleElement = document.querySelector('.title.ytmusic-player-bar');
     
     if (artistElement && titleElement) {
-        const newArtist = artistElement.textContent.trim();
-        const newTitle = titleElement.textContent.trim();
-        
+        var newArtist = artistElement.textContent.trim();
+        var newTitle  = titleElement.textContent.trim();
+
+        const copyartist=newArtist;
+        const copytitle=newTitle;
+
+        try {
+            //declared in ./YtMusic/Hubs.txt
+            if (hubs.includes(newArtist) && newTitle.includes("-")) {
+                let split = newTitle.split("-");
+
+                newArtist = split[0].split(",")[0].trim();
+                newTitle = split[1].trim();
+            }
+        } catch (e) {
+            console.warn(`Song cannot be resolved as multichannel (${copyartist} - ${copyTitle}):`, e);
+        }
+
         if (newArtist !== currentArtist || newTitle !== currentTitle) {
             console.log("Song changed:", currentTitle, "by", currentArtist, "->", newTitle, "by", newArtist);
+
             currentArtist = newArtist;
             currentTitle = newTitle;
+
             DecideBadge('90px', '50px', '.subtitle.ytmusic-player-bar a', '#left-controls > span', nextButton, 'auto', 'music');
         }
     }
